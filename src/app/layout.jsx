fa55846f75db6d9f09099ca7d978b988a552212e
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-13 13:06:07 +0530

    feat: icon added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,11 +6,17 @@ import { publicSans } from "@/components/ui/font";
 export const metadata = {
   title: "DulranDev",
   description: "FullStack Developer, ReactJS, NextJS, ExpressJS, TypeScript",
+  icons: {
+    icon: "/favicon.ico", // Add favicon icon
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${publicSans.className} antialiased bg-black max-w-2xl px-8 lg:px-0 mx-auto`}>
         <Navigation />
         {children}
